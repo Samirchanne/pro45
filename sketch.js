4d43a93fe04f703b99f37c73e5e466abe7dbdfec
Samirchanne <87372616+Samirchanne@users.noreply.github.com>
2021-11-29 21:32:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400,350,60,10);
+
+  createSprite(100,50,50,10);
+
+  createSprite(200,50,50,10);
+
+  createSprite(300,50,50,10);
+
+  createSprite(400,50,50,10);
+
+  createSprite(500,50,50,10);
+
+  createSprite(600,50,50,10);
+
+  createSprite(700,50,50,10);
+
+  createSprite(150,100,50,10);
+
+  createSprite(250,100,50,10);
+
+  createSprite(350,100,50,10);
+
+   createSprite(350,100,50,10);
+
+   createSprite(450,100,50,10);
+
+   createSprite(550,100,50,10);
+
+   createSprite(650,100,50,10);
+
+   createSprite(200,150,50,10);
+
+   createSprite(300,150,50,10);
+
+   createSprite(400,150,50,10);
+
+   createSprite(500,150,50,10);
+
+   createSprite(600,150,50,10);
+
+   createSprite(250,200,50,10);
+
+   createSprite(350,200,50,10);
+
+   createSprite(450,200,50,10);
+
+   createSprite(550,200,50,10);
+
+   createSprite(300,250,50,10);
+
+   createSprite(400,250,50,10);
+
+   createSprite(500,250,50,10);
+
+  createSprite(400,340,10,10);
+
+
+
+
+
+  
+
+}
+
+function draw() {
+  background(225,225,225);
+  
+
+  drawSprites();
+}
